44dfe1b0577a099ad0b87a83b25b13a7ecd7f43f
smigunov <mamonts@gmail.com>
2018-05-14 12:50:07 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,14 @@
 package ru.job4j.calculate; 
+/**
+	Учебный класс Calculate
+	@author Sergey Mugunov
+*/
 public class Calculate{
+	/**
+		Стартовая процедура main
+		@param args - args
+		@return Hello World
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello World");
 	}
