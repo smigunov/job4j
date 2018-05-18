89eaad4168548640f97d157541d5d0f755f61f2e
smigunov <mamonts@gmail.com>
2018-05-18 15:00:24 +0300

    Test Commit

--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -7,22 +7,22 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author Sergey Migunov
-* @version 1
-* @since 0.1
-*/
+ * Test.
+ *
+ * @author Sergey Migunov
+ * @version 1
+ * @since 0.1
+ */
 
 public class CalculateTest {
     /**
-    * Test echo.
-    */ 
+     * Test echo.
+     */
     @Test
 
     public void whenTakeNameThenTreeEchoPlusName() {
+        String expect = "Echo, echo, echo : Sergey Migunov";
         String input = "Sergey Migunov";
-        String expect = "Echo, echo, echo : Sergey Migunov"; 
         Calculate calc = new Calculate();
         String result = calc.echo(input);
         assertThat(result, is(expect));
