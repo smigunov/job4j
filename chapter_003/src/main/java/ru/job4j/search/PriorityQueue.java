95721b8c6a9a757e4e93b8ee05951ee902e94b24
smigunov <mamonts@gmail.com>
2018-07-12 16:46:23 +0300

    2. Очередь с приоритетом на LinkedList [#41670]

---
+++ chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -0,0 +1,33 @@
+package ru.job4j.search;
+
+import java.util.LinkedList;
+
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    /**
+     * Метод должен вставлять в нужную позицию элемент.
+     * Позиция определять по полю приоритет.
+     * Для вставик использовать add(int index, E value)
+     * @param task задача
+     */
+
+    private int getPriorityIndex(int priority) {
+        int index = 0;
+        for (int i = 0; i < this.tasks.size(); i++) {
+            if (this.tasks.get(i).getPriority() > priority) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    public void put(Task task) {
+        this.tasks.add(getPriorityIndex(task.getPriority()), task);
+    }
+
+    public Task take() {
+        return this.tasks.poll();
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PriorityQueueTest {
+    @Test
+    public void whenHigherPriority() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("low", 5));
+        queue.put(new Task("urgent", 1));
+        queue.put(new Task("middle", 3));
+        queue.put(new Task("middle2", 4));
+        Task result = queue.take();
+        assertThat(result.getDesc(), is("urgent"));
+    }
+
+    @Test
+    public void whenMiddlePriority() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("low", 5));
+        queue.put(new Task("urgent", 1));
+        queue.put(new Task("middle", 3));
+        Task result = queue.take();
+        result = queue.take();
+        assertThat(result.getDesc(), is("middle"));
+    }
+}
