39551cf89e1c91b4b942c8439e53d8f2cbc2e90e
smigunov <mamonts@gmail.com>
2018-07-30 08:32:26 +0300

    Отсортировать департаменты [#34352]

---
+++ chapter_003/src/test/java/ru/job4j/depart/DepartTest.java
@@ -0,0 +1,49 @@
+package ru.job4j.depart;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class DepartTest {
+    @Test
+    public void when3Departs() {
+        Organization dp = new Organization();
+        dp.addDepart("K1");
+        dp.addDepart("K1\\SK1");
+        dp.addDepart("K1\\SK1\\SSK1");
+
+        assertThat(dp.getDepartsArr(true), is(new String[] {"K1", "K1\\SK1", "K1\\SK1\\SSK1"}));
+    }
+    @Test
+    public void whenTopDepartIsNotExists() {
+        Organization dp = new Organization();
+        dp.addDepart("K1\\SK1");
+        dp.addDepart("K1\\SK1\\SSK1");
+        assertThat(dp.getDepartsArr(true), is(new String[] {"K1", "K1\\SK1", "K1\\SK1\\SSK1"}));
+    }
+    @Test
+    public void whenMiddleDepartIsNotExists() {
+        Organization dp = new Organization();
+        dp.addDepart("K1");
+        dp.addDepart("K1\\SK1\\SSK1");
+        assertThat(dp.getDepartsArr(true), is(new String[] {"K1", "K1\\SK1", "K1\\SK1\\SSK1"}));
+    }
+
+    @Test
+    public void when2TopDeparts() {
+        Organization dp = new Organization();
+        dp.addDepart("K1\\SK1\\SSK1");
+        dp.addDepart("K2\\SK1\\SSK1");
+        assertThat(dp.getDepartsArr(true), is(new String[] {"K1", "K1\\SK1", "K1\\SK1\\SSK1", "K2", "K2\\SK1", "K2\\SK1\\SSK1"}));
+    }
+
+    @Test
+    public void whenDescending() {
+        Organization dp = new Organization();
+        dp.addDepart("K1\\SK1");
+        dp.addDepart("K1\\SK2");
+        dp.addDepart("K2\\SK1");
+        dp.addDepart("K2\\SK2");
+        assertThat(dp.getDepartsArr(false), is(new String[] {"K2", "K2\\SK2", "K2\\SK1", "K1", "K1\\SK2", "K1\\SK1"}));
+    }
+}
