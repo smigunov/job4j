dd3931159241898e75fada8875a3b211d1b9b5b9
smigunov <mamonts@gmail.com>
2018-08-11 13:20:54 +0300

    5.1.4. Создать convert(Iterator<Iterator>) [#152]

---
+++ chapter_001/src/main/java/ru/job4j/level_2/iterator/Convert.java
@@ -0,0 +1,44 @@
+package ru.job4j.level_2.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Convert {
+    Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
+        return new Iterator<Integer>() {
+            Iterator<Integer> currentIterator;
+
+            private void setCurrentIterator() {
+                if ((currentIterator == null) || (!currentIterator.hasNext())) {
+                    if (it.hasNext()) {
+                        currentIterator = it.next();
+                    } else {
+                        currentIterator = null;
+                    }
+                }
+            }
+
+            @Override
+            public boolean hasNext() {
+                boolean result = false;
+                setCurrentIterator();
+                if (currentIterator != null) {
+                    result = currentIterator.hasNext();
+                }
+                return result;
+            }
+
+            @Override
+            public Integer next() {
+                Integer result = 0;
+                setCurrentIterator();
+                if (currentIterator != null) {
+                    result = currentIterator.next();
+                } else {
+                    throw new NoSuchElementException();
+                }
+                return result;
+            }
+        };
+    }
+}
--- chapter_001/src/test/java/ru/job4j/level_2/iterator/EvenNumIteratorTest.java
+++ chapter_001/src/test/java/ru/job4j/level_2/iterator/EvenNumIteratorTest.java
@@ -1,6 +1,10 @@
 package ru.job4j.level_2.iterator;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -34,5 +38,52 @@ public class EvenNumIteratorTest {
         it.next();
         boolean b = it.hasNext();
         assertThat(b, is(false));
+    }
+
+    private Iterator<Integer> it;
+
+    @Before
+    public void setUp(){
+        it = new EvenNumIterator(new Integer[]{1, 2, 3, 4, 5, 6, 7});
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void shouldReturnEvenNumbersSequentially () {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(6));
+        assertThat(it.hasNext(), is(false));
+        it.next();
+    }
+
+    @Test
+    public void sequentialHasNextInvocationDoesntAffectRetrievalOrder () {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(6));
+    }
+
+    @Test
+    public void  shouldReturnFalseIfNoAnyEvenNumbers(){
+        it = new EvenNumIterator(new Integer[]{1});
+        assertThat(it.hasNext(), is(false));
+    }
+
+    @Test
+    public void allNumbersAreEven(){
+        it = new EvenNumIterator(new Integer[]{2,4,6,8});
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(6));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(8));
     }
 }
