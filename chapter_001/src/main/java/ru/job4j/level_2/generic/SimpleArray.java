451ecbc5a01dab76dae2e97bcf364b44429ea644
smigunov <mamonts@gmail.com>
2018-08-14 11:37:38 +0300

    5.2.1. Реализовать SimpleArray<T> [#156]

---
+++ chapter_001/src/main/java/ru/job4j/level_2/generic/SimpleArray.java
@@ -0,0 +1,64 @@
+package ru.job4j.level_2.generic;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SimpleArray<T> implements Iterable<T> {
+    private Object[] values ;
+    private int idx = 0;
+    private int length;
+    public void add(T newValue) {
+        this.values[this.idx] = newValue;
+        this.idx++;
+    }
+    public SimpleArray(int length) {
+        this.values = new Object[length];
+        this.length = length;
+    }
+
+    public void set(int index, T newValue) {
+        this.values[index] = newValue;
+    }
+
+    public void delete(int index) {
+        if (index >= 0) {
+            for (int i = index + 1; i < values.length; i++) {
+                this.values[i - 1] = values[i];
+            }
+            this.length--;
+        }
+    }
+
+    public T get(int index) {
+        T result = null;
+        if (index < this.values.length) {
+            result =  (T)this.values[index];
+        } else {
+            throw new NoSuchElementException();
+        }
+        return result;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new SimpleArrayIterator<T>();
+    }
+
+    private class SimpleArrayIterator<T> implements Iterator<T> {
+        private int idx = 0;
+        @Override
+        public boolean hasNext () {
+            return this.idx < SimpleArray.this.length;
+        }
+
+        @Override
+        public T next () {
+            if (this.idx < SimpleArray.this.length) {
+                this.idx++;
+                return (T)SimpleArray.this.values[this.idx - 1];
+            } else {
+                throw new NoSuchElementException();
+            }
+        }
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/level_2/generic/SimpleArrayTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.level_2.generic;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+
+public class SimpleArrayTest {
+    @Test
+    public void whenAdd1ValueThenGetThisValue() {
+        SimpleArray<Integer> sa = new SimpleArray<>(1);
+        sa.add(555);
+        assertThat(sa.get(0), is(555));
+    }
+
+    @Test
+    public void whenDeleteThenIterateCountReducedByOne() {
+        SimpleArray<Integer> sa = new SimpleArray<>(4);
+        sa.add(1);
+        sa.add(2);
+        sa.add(3);
+        sa.add(4);
+        sa.delete(2);
+        List<Integer> lst = new ArrayList<>();
+        for (Integer itemValue : sa) {
+            lst.add(itemValue);
+        }
+        assertThat(lst.toArray(), is(new Integer[]{1, 2, 4}));
+    }
+
+}
