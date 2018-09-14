9aca79324e8fa18396247e2179333e65f2947948
smigunov <mamonts@gmail.com>
2018-09-14 17:41:43 +0300

    5.3.4. Задан связанный список. Определить цикличность. [#161][#71284]. Исправление 1.

--- level2/src/main/java/ru/job4j/level2/list/CycleNode.java
+++ level2/src/main/java/ru/job4j/level2/list/CycleNode.java
@@ -3,30 +3,21 @@ package ru.job4j.level2.list;
 public class CycleNode<T> {
     T value;
     CycleNode<T> next;
-    private boolean alreadyChecked = false;
-
-    private void FlushLabels() {
-        CycleNode<T> node = this;
-        while((node.next != null) &&  (node.alreadyChecked))  {
-            node.alreadyChecked = false;
-            node = node.next;
-        }
-    }
 
 
     public boolean hasCycle() {
         boolean result = false;
-        this.FlushLabels();
-
-        CycleNode<T> curNode = this;
-        while(curNode.next != null) {
-            curNode.alreadyChecked = true;
-            if (curNode.next.alreadyChecked) {
-                result = true;
-                break;
+        CycleNode<T> tortoise = this;
+        if (this.next != null) {
+            CycleNode<T> rabbit = this.next;
+            while (rabbit != tortoise && rabbit.next != null && rabbit.next.next != null && tortoise != null) {
+                tortoise = tortoise.next;
+                rabbit = rabbit.next.next;
+                if (rabbit == tortoise) {
+                    result = true;
+                    break;
+                }
             }
-            curNode.alreadyChecked = true;
-            curNode = curNode.next;
         }
         return result;
     }
