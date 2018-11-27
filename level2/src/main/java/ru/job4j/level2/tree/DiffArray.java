3c20644a801cc537d5eeb8a9b4df62482548c608
smigunov <mamonts@gmail.com>
2018-11-27 17:54:29 +0300

    Разделить массив чисел на 2 близких по сумме

--- level2/src/main/java/ru/job4j/level2/tree/DiffArray.java
+++ level2/src/main/java/ru/job4j/level2/tree/DiffArray.java
@@ -1,26 +1,44 @@
 package ru.job4j.level2.tree;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class DiffArray {
-    public static void diff(int[] a, int[] b, int[] c) {
-        if (a.length > 0) {
-            Arrays.sort(a);
+    ArrayList<Integer> a;
+    ArrayList<Integer> b;
+    public DiffArray() {
+        a = new ArrayList<Integer>();
+        b = new ArrayList<Integer>();
+    }
+
+    public ArrayList<Integer> getA() {
+        return this.a;
+    }
+
+    public ArrayList<Integer> getB() {
+        return this.b;
+    }
+
+    public static DiffArray diff(int[] src) {
+        DiffArray diffArr = new DiffArray();
+
+        if (src.length > 0) {
+            Arrays.sort(src);
             int sum1 = 0;
             int sum2 = 0;
             int i1 = 0;
             int i2 = 0;
-            for (int i = a.length - 1; i >= 0; i--) {
+            for (int i = src.length - 1; i >= 0; i--) {
                 if (sum1 < sum2) {
-                    b[i1] = a[i];
-                    i1++;
-                    sum1 += a[i];
+                    diffArr.a.add(src[i]);
+                    sum1 += src[i];
                 } else {
-                    c[i2] = a[i];
-                    i2++;
-                    sum2 += a[i];
+                    diffArr.b.add(src[i]);
+                    sum2 += src[i];
                 }
             }
         }
+
+        return diffArr;
     }
 }
--- level2/src/test/java/ru/job4j/level2/tree/DiffArrayTest.java
+++ level2/src/test/java/ru/job4j/level2/tree/DiffArrayTest.java
@@ -8,28 +8,22 @@ import static org.junit.Assert.*;
 public class DiffArrayTest {
     @Test
     public void test1() {
-        int[] b = new int[4];
-        int[] c = new int[4];
-        DiffArray.diff(new int[] {1, 2, 5, 10}, b, c);
-        assertThat(b, is(new int[] {5, 2, 1, 0}));
-        assertThat(c, is(new int[] {10, 0, 0, 0}));
+        DiffArray difArr = DiffArray.diff(new int[] {1, 2, 5, 10});
+        assertThat(difArr.getA().toArray(), is(new Integer[] {5, 2, 1}));
+        assertThat(difArr.getB().toArray(), is(new Integer[] {10}));
     }
 
     @Test
     public void test2() {
-        int[] b = new int[4];
-        int[] c = new int[4];
-        DiffArray.diff(new int[] {3, 4, 8, 10}, b, c);
-        assertThat(b, is(new int[] {8, 4, 0, 0}));
-        assertThat(c, is(new int[] {10, 3, 0, 0}));
+        DiffArray difArr = DiffArray.diff(new int[] {3, 4, 8, 10});
+        assertThat(difArr.getA().toArray(), is(new Integer[] {8, 4}));
+        assertThat(difArr.getB().toArray(), is(new Integer[] {10, 3}));
     }
 
     @Test
     public void test3() {
-        int[] b = new int[4];
-        int[] c = new int[4];
-        DiffArray.diff(new int[] {3, 4, 8, 3}, b, c);
-        assertThat(b, is(new int[]{4, 3, 3, 0}));
-        assertThat(c, is(new int[]{8, 0, 0, 0}));
+        DiffArray difArr = DiffArray.diff(new int[] {3, 4, 8, 3});
+        assertThat(difArr.getA().toArray(), is(new Integer[]{4, 3, 3}));
+        assertThat(difArr.getB().toArray(), is(new Integer[]{8}));
     }
 }
